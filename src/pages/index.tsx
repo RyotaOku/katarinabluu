6252ad9d7b42e1480475dbd924f3bf57e21d2f58
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-04-25 10:36:29 +0900

    test change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 export default function Main() {
-    return (
-        <>
-            <p>開発中</p>
-            <button onClick={() => {
-                // window.open('https://localhost:50443/introduction', '_self');
-            }}>クリックしてね</button>
-            <h1>Ohayou</h1>
-        </>
-    )
+  return (
+    <>
+      <p>開発中</p>
+      <button
+        onClick={() => {
+          // window.open('https://localhost:50443/introduction', '_self');
+        }}
+      >
+        クリックしてね
+      </button>
+      <h1>Ohayou</h1>
+      <p>Text</p>
+    </>
+  );
 }
