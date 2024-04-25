d45526e2da25f8e1a468f4740baa6a94f14030c8
RyotaOku <ryota1991o@gmail.com>
2024-04-25 09:55:25 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <>
+            <p>開発中</p>
+            <button onClick={() => {
+                window.open('https://localhost:50443/introduction', '_self');
+            }}>クリックしてね</button>
+        </>
+    )
+}
