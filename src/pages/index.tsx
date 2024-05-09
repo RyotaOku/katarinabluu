60812a6c46945a8449223808aee59ec7952b1d56
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-05-09 11:51:50 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 export default function Main() {
-    return (
-        <>
-            <p>開発中</p>
-            <button onClick={() => {
-                // window.open('https://localhost:50443/introduction', '_self');
-            }}>クリックしてね</button>
-        </>
-    )
+  return (
+    <>
+      <p>開発中</p>
+      <button
+        onClick={() => {
+          // window.open('https://localhost:50443/introduction', '_self');
+        }}
+      >
+        クリックしてね
+      </button>
+      <h2>new chat</h2>
+    </>
+  );
 }
