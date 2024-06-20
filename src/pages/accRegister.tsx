c0fa02c2cc7df9de0e39618deb5c2f5ecc61c3b6
AarJul <barongdong3@gmail.com>
2024-06-20 11:16:19 +0900

    feat: account touroku

--- src/pages/accRegister.tsx
+++ src/pages/accRegister.tsx
@@ -1,14 +1,90 @@
-// pages/accRegister.tsx
 import React from 'react';
+import styles from '../styles/accRegister.module.css'; // Import CSS Module
 
-const AccRegisterPage: React.FC = () => {
+const AccRegister: React.FC = () => {
+  const [userName, setUserName] = React.useState('');
+  const [gender, setGender] = React.useState('');
+  const [birthday, setBirthday] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Implement form submission logic here
+    console.log('Form submitted:', { userName, gender, birthday, password }); // Example logging
+  };
+
   return (
-    <div>
-      <h1>Account Registration</h1>
-      <p>Welcome to the account registration page!</p>
-      {/* Add your registration form and logic here */}
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className={styles.formContainer}
+    >
+      <label
+        htmlFor="userName"
+        className={styles.formLabel}
+      >
+        ユーザーネーム
+      </label>
+      <input
+        type="text"
+        id="userName"
+        name="userName"
+        value={userName}
+        onChange={(event) => setUserName(event.target.value)}
+        className={styles.formInput}
+      />
+      <label
+        htmlFor="gender"
+        className={styles.formLabel}
+      >
+        性別
+      </label>
+      <select
+        id="gender"
+        name="gender"
+        value={gender}
+        onChange={(event) => setGender(event.target.value)}
+        className={styles.formSelect}
+      >
+        <option value="">未選択</option>
+        <option value="male">男性</option>
+        <option value="female">女性</option>
+      </select>
+      <label
+        htmlFor="birthday"
+        className={styles.formLabel}
+      >
+        生年月日:
+      </label>
+      <input
+        type="date"
+        id="birthday"
+        name="birthday"
+        value={birthday}
+        onChange={(event) => setBirthday(event.target.value)}
+        className={styles.formInput}
+      />
+      <label
+        htmlFor="password"
+        className={styles.formLabel}
+      >
+        パスワード
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        className={styles.formInput}
+      />
+      <button
+        type="submit"
+        className={styles.submitButton}
+      >
+        登録
+      </button>
+    </form>
   );
 };
 
-export default AccRegisterPage;
+export default AccRegister;
