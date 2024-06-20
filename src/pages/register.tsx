6e50977d87e9e7bad1c95a7c12dd44fb6c063b4f
Ivan <ivankurniawan2012@gmail.com>
2024-06-20 10:41:02 +0900

    chore:

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -49,16 +49,16 @@ const RegisterPage: React.FC = () => {
     </div>
   );
 };
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-export default RegisterPage;
+// const auth = getAuth();
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed up
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
+// export default RegisterPage;
