6b2ed85411d96dc6f98b44a7302c9081bf6329e6
AarJul <barongdong3@gmail.com>
2024-06-24 15:22:32 +0900

    chore: other pages add

---
+++ src/pages/statistics.tsx
@@ -0,0 +1,28 @@
+// pages/index.tsx
+import React from 'react';
+import Head from 'next/head';
+import Navigation from '@/components/navigation';
+
+const Statistics: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Calendar App</title>
+        <meta
+          name="description"
+          content="あなたのジョブシフト"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
+      <Navigation />
+      <main>
+        <p>lol</p>
+      </main>
+    </div>
+  );
+};
+
+export default Statistics;
