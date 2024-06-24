3ae1c3682bdae9957c1e553308138264170cb06b
AarJul <barongdong3@gmail.com>
2024-06-24 16:12:46 +0900

    feat: COMMENTING OUT all sessions

--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -10,13 +10,13 @@ const Statistics: React.FC = () => {
 
   useEffect(() => {
     // User verification
-    const userId = getUserSession();
-    if (userId) {
-      setUserId(userId);
-    } else {
-      // Handle case where user is not logged in, e.g., redirect to login
-      router.push('/index');
-    }
+    // const userId = getUserSession();
+    // if (userId) {
+    //   setUserId(userId);
+    // } else {
+    //   // Handle case where user is not logged in, e.g., redirect to login
+    //   router.push('/index');
+    // }
   }, []);
   return (
     <div>
