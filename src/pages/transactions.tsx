5a5a4aeb1a79b11ecf9370b5e345b5d5d6304689
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-07-25 10:17:10 +0900

    chore: styling

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -203,13 +203,15 @@ const Transactions: React.FC = () => {
       {
         label: '収入',
         data: yearlyIncome,
-        backgroundColor: 'var(--blue)',
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1,
       },
       {
         label: '支出',
         data: yearlyExpense,
-        backgroundColor: 'var(--red)',
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
       },
     ],
